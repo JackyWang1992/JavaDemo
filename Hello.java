85a89581fc108f384beeab65116052024966f267
JackyWang <jackywang@git.com>
2017-11-20 10:34:17 -0500

    added Japanese word

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Haha@@@@");
   }
-
+  public static void printJapanese(){
+    System.out.println("山");
+  }
 }
